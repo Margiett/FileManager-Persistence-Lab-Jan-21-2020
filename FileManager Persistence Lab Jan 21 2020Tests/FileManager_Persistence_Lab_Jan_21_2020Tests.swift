7a453eee8058cc5782b28f52aa2664865cc9dcbb
Margiett Gil <mxg627@icloud.com>
2020-01-21 18:18:14 -0500

    Initial Commit

---
+++ FileManager Persistence Lab Jan 21 2020Tests/FileManager_Persistence_Lab_Jan_21_2020Tests.swift
@@ -0,0 +1,34 @@
+//
+//  FileManager_Persistence_Lab_Jan_21_2020Tests.swift
+//  FileManager Persistence Lab Jan 21 2020Tests
+//
+//  Created by Margiett Gil on 1/21/20.
+//  Copyright © 2020 Margiett Gil. All rights reserved.
+//
+
+import XCTest
+@testable import FileManager_Persistence_Lab_Jan_21_2020
+
+class FileManager_Persistence_Lab_Jan_21_2020Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
